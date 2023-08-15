16d9abffdce8accd0f26209fe81ce118105459f9
Jessy Benitez <jessybntz@gmail.com>
2023-08-15 16:22:39 -0400

    demo to test hashmap of genres

--- demo.java
+++ demo.java
@@ -1,7 +1,15 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.util.*;
 public class demo
 {
     public static void main(String[] args)
     {
+        HashMap<String, String> genresMap = genres.genreDB();       
+        for(Map.Entry<String, String> entry : genresMap.entrySet())
+            System.out.println(entry.getKey() + ": " + entry.getValue());
         
     }
 }
