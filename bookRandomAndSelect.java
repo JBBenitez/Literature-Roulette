31919d3798cfec8121b25deede94083572080397
Jessy Benitez <jessybntz@gmail.com>
2023-08-27 16:46:28 -0400

    ganged googlebooks api to openlibrary api

--- bookRandomAndSelect.java
+++ bookRandomAndSelect.java
@@ -6,14 +6,22 @@ import java.net.URL;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import java.lang.Math;
 public class bookRandomAndSelect {
-    public static void main(String[] args)
+    public static Object[] getRandomBooks(String genre)
     {
-        String apiUrl = "https://openlibrary.org/subjects/science_fiction.json?details=false";
-        int resultsPerPage = 1;  // Number of results per page (Open Library max limit)
-
+        int max = 1000;
+        int min = 0;
+        int books = 0;
+        int range = max - min + 1;
+        
+        int rand = (int)(Math.random()*range)+min;
+        JSONObject[] works = new JSONObject [5];
+        String apiUrl = "https://openlibrary.org/subjects/"+genre+".json?details=false";
+        int resultsPerPage = 5;  // Number of results per page (Open Library max limit)
+        
         try {
-            String apiUrlWithPagination = apiUrl + "&limit=" + resultsPerPage;
+            String apiUrlWithPagination = apiUrl + "&limit=" + resultsPerPage + "&offset="+rand;
             URL url = new URL(apiUrlWithPagination);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
@@ -26,12 +34,21 @@ public class bookRandomAndSelect {
                 response.append(line);
             }
             reader.close();
+            Object obj = new JSONParser().parse(response.toString());
+            JSONObject jResp = (JSONObject) obj;
+            JSONArray jRespArray = (JSONArray) jResp.get("works");
+            for( Object workObj: jRespArray )
+            {
+                JSONObject work = (JSONObject) workObj;
+                works[books++] = work;
+            }
+
 
             // Print the raw JSON response
-            System.out.println(response.toString());
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return works;
     }
 }
