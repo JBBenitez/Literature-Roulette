34870a90358978f7649766ac261aa890e54611f2
Jessy Benitez <jessybntz@gmail.com>
2023-08-27 16:46:44 -0400

    more testing

--- demo.java
+++ demo.java
@@ -3,13 +3,28 @@ import java.sql.DriverManager;
 import java.sql.Statement;
 import java.sql.ResultSet;
 import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import java.lang.Math;
 public class demo
 {
     public static void main(String[] args)
     {
-        HashMap<String, String> genresMap = genres.genreDB();       
-        for(Map.Entry<String, String> entry : genresMap.entrySet())
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        
+        JSONObject[] works;
+        String genre = "horror";
+        works = (JSONObject[]) bookRandomAndSelect.getRandomBooks(genre);
+        System.out.println(works.length);
+        for( Object workObj: works)
+            {
+                JSONObject work = (JSONObject) workObj;
+                String title = (String) work.get("title");
+                System.out.println("Title: " + title);
+            }
     }
 }
